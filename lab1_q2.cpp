f1b95b341db84cf7803b0fa89a3dafac0e397cc6
suyash-22103261 <22103261@mail.jiit.ac.in>
2024-07-27 09:49:49 +0530

    lab1_q2.cpp

--- lab1_q2.cpp
+++ lab1_q2.cpp
@@ -4,15 +4,20 @@ using namespace std;
 int main()
 {
     int n,i;
-    float s;
     cout<<"Enter the no. of numbers : ";
     cin>>n;
+    float s[n];
     float sum=0;
     for(i=0;i<n;i++)
     {
-        cin>>s;
-        sum+=s;
+        cin>>s[i];
+        sum+=s[i];
     }
-    cout<<"Average of numbers : "<<sum/n;
+    cout<<"Array of numbers : ";
+    for(i=0;i<n;i++)
+    {
+        cout<<s[i];
+    }
+    cout<<"\nAverage of numbers : "<<sum/n;
     return 0;
 }
